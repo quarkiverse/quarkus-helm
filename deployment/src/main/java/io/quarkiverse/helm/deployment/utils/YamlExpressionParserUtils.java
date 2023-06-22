925b9bdbd3a74aa89f0b8faf7441560538a9c58d
Jose <josecarvajalhilario@gmail.com>
2023-06-22 14:09:24 +0200

    Document how to map properties using Quarkus K8s Config extension
    fix https://github.com/quarkiverse/quarkus-helm/issues/261

--- deployment/src/main/java/io/quarkiverse/helm/deployment/utils/YamlExpressionParserUtils.java
+++ deployment/src/main/java/io/quarkiverse/helm/deployment/utils/YamlExpressionParserUtils.java
@@ -1,8 +1,11 @@
 package io.quarkiverse.helm.deployment.utils;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+import io.dekorate.ConfigReference;
+import io.dekorate.utils.Strings;
 import io.github.yamlpath.YamlExpressionParser;
 
 public final class YamlExpressionParserUtils {
@@ -11,6 +14,11 @@ public final class YamlExpressionParserUtils {
     public static final String SEPARATOR_QUOTES = ":DOUBLE_QUOTES";
     public static final String START_EXPRESSION_TOKEN = ":START:";
     public static final String END_EXPRESSION_TOKEN = ":END:";
+    public static final String START_TAG = "{{";
+    public static final String END_TAG = "}}";
+    public static final String EMPTY = "";
+    public static final String VALUES_START_TAG = START_TAG + " .Values.";
+    public static final String VALUES_END_TAG = " " + END_TAG;
 
     private YamlExpressionParserUtils() {
 
@@ -28,6 +36,24 @@ public final class YamlExpressionParserUtils {
     public static Object readAndSet(YamlExpressionParser parser, String path, String expression) {
         Set<Object> found = parser.readAndReplace(path, adaptExpression(expression));
         return found.stream().findFirst().orElse(null);
+    }
+
+    public static String toExpression(String property, Object provided, Object found, ConfigReference valueReference) {
+        Optional<String> expressionProvided = Optional.ofNullable(valueReference.getExpression())
+                .filter(Strings::isNotNullOrEmpty);
+
+        if (expressionProvided.isPresent()) {
+            return expressionProvided.get();
+        }
+
+        String conversion = EMPTY;
+        // we only need to quote when the found value in the generated resources is a string, but the provided type isn't.
+        if (provided != null && !(provided instanceof String) && found instanceof String) {
+            // we need conversion
+            conversion = " | quote";
+        }
+
+        return VALUES_START_TAG + property + conversion + VALUES_END_TAG;
     }
 
     private static String adaptExpression(String expression) {
